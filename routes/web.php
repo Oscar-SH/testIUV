c0b49538a86f9aa2ab6f68c1bcc249e59da2dc54
Oscar-SH <oscrsnts29@gmail.com>
2025-03-23 22:50:17 -0600

    Iniciando proyecto

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    if (auth()->check()) {
+        return Inertia::render('Dashboard');
+    } else {
+        return redirect()->route('login');
+    }
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
